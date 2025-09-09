d6bacae7c153f9a520609d14c24dceaf382304bc
Christian <chris_cobar@outlook.com>
2025-09-09 16:44:41 -0700

    Adding routing through react-router-dom. Created the pages folder, which includes the pages we currently have, NotesPage, NotFoundPage, and PrivacyPage. Also added a Privacy link to the NavBar and made the NavBar title clickable, routed to the main page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import LoginModal from "./components/LogInModal";
 import NavBar from "./components/NavBar";
 import SignUpModal from "./components/SignUpModal";
-import styles from "./styles/NotesPage.module.css";
-import { useEffect, useState } from "react";
 import { User } from "./models/user";
 import * as NotesAPI from "./network/notes_api";
-import NotesPageLoggedInView from "./components/NotesPageLoggedInView";
-import NotesPageLoggedOutView from "./components/NotesPageLoggedOutView";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NotesPage from "./pages/NotesPage";
+import PrivacyPage from "./pages/PrivacyPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import styles from "./styles/App.module.css"
 
 function App() {
 
@@ -30,40 +32,50 @@ function App() {
     // MAIN RENDER
     // --------------------------------------------------------------------------
     return (
-        <div>
-            <NavBar
-                loggedInUser={loggedInUser}
-                onLoginClicked={() => { setShowLoginModal(true) }}
-                onSignUpClicked={() => { setShowSignUpModal(true) }}
-                onLogoutSuccessfull={() => { setLoggedInUser(null) }}
-            />
-            <Container className={styles.notesPage}>
-                <>
-                { loggedInUser
-                    ? <NotesPageLoggedInView />
-                    : <NotesPageLoggedOutView />
+        <BrowserRouter>
+            <div>
+                <NavBar
+                    loggedInUser={loggedInUser}
+                    onLoginClicked={() => { setShowLoginModal(true) }}
+                    onSignUpClicked={() => { setShowSignUpModal(true) }}
+                    onLogoutSuccessfull={() => { setLoggedInUser(null) }}
+                />
+                <Container className={styles.pageContainer}>
+                    <Routes>
+                        <Route 
+                        path='/'
+                        element={<NotesPage loggedInUser={loggedInUser} />}
+                        />
+                        <Route 
+                        path='/privacy'
+                        element={<PrivacyPage />}
+                        />
+                        <Route 
+                        path='/*'
+                        element={<NotFoundPage />}
+                        />
+                    </Routes>
+                </Container>
+                {showSignUpModal &&
+                        <SignUpModal
+                            onDismiss={() => { setShowSignUpModal(false) }}
+                            onSignUpSuccesful={(user) => { 
+                            setLoggedInUser(user);
+                            setShowSignUpModal(false);
+                            }}
+                        />
                 }
-                </>
-            </Container>
-            {showSignUpModal &&
-                    <SignUpModal
-                        onDismiss={() => { setShowSignUpModal(false) }}
-                        onSignUpSuccesful={(user) => { 
-                        setLoggedInUser(user);
-                        setShowSignUpModal(false);
+                {showLoginModal &&
+                    <LoginModal
+                        onDismiss={() => { setShowLoginModal(false) }}
+                        onLoginSuccesful={(user) => {
+                            setLoggedInUser(user);
+                            setShowLoginModal(false);
                         }}
                     />
-            }
-            {showLoginModal &&
-                <LoginModal
-                    onDismiss={() => { setShowLoginModal(false) }}
-                    onLoginSuccesful={(user) => {
-                        setLoggedInUser(user);
-                        setShowLoginModal(false);
-                    }}
-                />
-            }
-        </div>
+                }
+            </div>
+        </BrowserRouter>
     );
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { User } from "../models/user";
 import NavBarLoggedInView from "./NavBarLoggedInView";
 import NavBarLoggedOutView from "./NavBarLoggedOutView";
+import { Link } from "react-router-dom";
 
 interface NavBarProps {
     loggedInUser: User | null,
@@ -14,11 +15,16 @@ const NavBar = ({loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccessf
     return ( 
         <Navbar bg="primary" variant="dark" expand="md" >
             <Container>
-                <Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">
                     SpottDaDiff
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-navbar" />
                 <Navbar.Collapse id="main-navbar">
+                    <Nav>
+                        <Nav.Link as={Link} to="/privacy">
+                            Privacy                            
+                        </Nav.Link>
+                    </Nav>
                     <Nav className="ms-auto">
                         { loggedInUser
                         ? <NavBarLoggedInView user={loggedInUser} onLogoutSuccessfull={onLogoutSuccessfull}/>
