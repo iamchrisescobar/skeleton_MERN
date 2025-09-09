0aa8e2c71f0a9c0ca4b3442cf84f92657ef20e70
Christian <chris_cobar@outlook.com>
2025-09-09 12:56:25 -0700

    adding user model to frontend and apis to log in, sign up, log out, and check session.

--- frontend/src/network/notes_api.tsx
+++ frontend/src/network/notes_api.tsx
@@ -1,4 +1,5 @@
 import { Note } from "../models/note";
+import { User } from "../models/user";
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
     const response = await fetch(input, init);
@@ -11,8 +12,55 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
     }
 }
 
+// NOTE: this can be done since front and backend are on same url. So the cookies will be sent automatically.
+// If they were on different domains or subdomains we would have to include the credentials explicitly,
+// which is done in the fetchData configuration
+export async function getLoggedInUser(): Promise<User> {
+    const response = await fetchData("api/users", {method: "GET"});
+    return response.json();
+}
+
+export interface SingUpCredentials {
+    usermame: string,
+    email: string,
+    password: string,
+}
+
+export async function signUp(credentials:SingUpCredentials): Promise<User> {
+    const response = await fetchData("api/users/signup",
+    {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+    });
+    return response.json();
+}
+
+export interface LoginCredentials {
+    usermame: string,
+    password: string,
+}
+
+export async function login(credentials:LoginCredentials): Promise<User> {
+    const response = await fetchData("api/users/login",
+    {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+    });
+    return response.json();
+}
+
+export async function logout() {
+    await fetchData("/api/users/logout", { method: "POST" });
+}
+
 export async function fetchNotes(): Promise<Note[]> {
-    const response = await fetchData("/api/notes", {method: "GET"});
+    const response = await fetchData("/api/notes", { method: "GET" });
     return response.json();    
 }
 
