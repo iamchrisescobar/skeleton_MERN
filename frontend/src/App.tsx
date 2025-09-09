9a4174f5d3df65680942701aba991566d38a6543
Christian <chris_cobar@outlook.com>
2025-09-08 18:18:06 -0700

    formatting App.tsx to have 4 spaces for indentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,87 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { Note as NoteModel } from './models/note';
-import { Button, Col, Container, Row} from 'react-bootstrap';
-import Note from './components/Note';
+import React, { useEffect, useState } from "react";
+import { Note as NoteModel } from "./models/note";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Note from "./components/Note";
 import styles from "./styles/NotesPage.module.css";
 import styleUtils from "./styles/utils.module.css";
 import * as NotesAPI from "./network/notes_api";
-import AddEditNotesDialog from './components/AddEditNotesDialog';
+import AddEditNotesDialog from "./components/AddEditNotesDialog";
 import { FaPlus } from "react-icons/fa";
 
 function App() {
-  const [notes, setNotes] = useState<NoteModel[]>([]);
+    const [notes, setNotes] = useState<NoteModel[]>([]);
 
-  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+    const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
-  const [noteToEdit, setNoteToEdit] = useState<NoteModel|null>(null);
+    const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
-  useEffect(() => {
-    async function loadNotes() {
-      try {
-        const notes = await NotesAPI.fetchNotes();
-        setNotes(notes);
-      } catch (error) {
-        console.error(error);
-        alert(error);
-      }
+    useEffect(() => {
+        async function loadNotes() {
+            try {
+                const notes = await NotesAPI.fetchNotes();
+                setNotes(notes);
+            } catch (error) {
+                console.error(error);
+                alert(error);
+            }
+        }
+        loadNotes();
+    }, []); // if you want something to exec 1 time, pass an empty array for dependency list
+
+    async function deleteNote(note: NoteModel) {
+        try {
+            await NotesAPI.deleteNote(note._id);
+            setNotes(notes.filter((existingNote) => existingNote._id !== note._id));
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
     }
-    loadNotes();
-  }, []); // if you want something to exec 1 time, pass an empty array for dependency list 
 
-  async function deleteNote(note: NoteModel) {
-    try {
-      await NotesAPI.deleteNote(note._id);
-      setNotes(notes.filter(existingNote => existingNote._id !== note._id));
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    }
-  }
-  
-  return (
-    <Container>
-      <Button 
-        className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexcenter}`}
-        onClick={() => setShowAddNoteDialog(true)}>
-        <FaPlus />
-        Add New Note
-      </Button>
-      <Row xs={1} md={2} xl={3} className='g-4'>
-        {notes.map(note => (
-          <Col key={note._id}>
-            <Note 
-              note={note}
-              onNoteClicked={setNoteToEdit}
-              onDeleteNoteClick={deleteNote}
-              className={styles.note} 
-            />
-          </Col>
-          
-        ))}
-      </Row>
-      { showAddNoteDialog &&
-        <AddEditNotesDialog 
-          onDismiss={() => setShowAddNoteDialog(false)}
-          onNoteSaved={(newNote) => {
-            setNotes([...notes, newNote]);
-            setShowAddNoteDialog(false);
-
-          }}
-        />
-
-      }
-      { noteToEdit &&
-        <AddEditNotesDialog
-        noteToEdit={noteToEdit} 
-        onDismiss={() => setNoteToEdit(null)}
-        onNoteSaved={(updatedNote) => {
-          setNotes(notes.map(existingNote => existingNote._id === updatedNote._id ? updatedNote : existingNote));
-          setNoteToEdit(null);
-        }}
-        />
-      }
-    </Container>
-  );
+    return (
+        <Container>
+            <Button
+                className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexcenter}`}
+                onClick={() => setShowAddNoteDialog(true)}
+            >
+                <FaPlus />
+                Add New Note
+            </Button>
+            <Row xs={1} md={2} xl={3} className="g-4">
+                {notes.map((note) => (
+                    <Col key={note._id}>
+                        <Note
+                            note={note}
+                            onNoteClicked={setNoteToEdit}
+                            onDeleteNoteClick={deleteNote}
+                            className={styles.note}
+                        />
+                    </Col>
+                ))}
+            </Row>
+            {showAddNoteDialog && (
+                <AddEditNotesDialog
+                    onDismiss={() => setShowAddNoteDialog(false)}
+                    onNoteSaved={(newNote) => {
+                        setNotes([...notes, newNote]);
+                        setShowAddNoteDialog(false);
+                    }}
+                />
+            )}
+            {noteToEdit && (
+                <AddEditNotesDialog
+                    noteToEdit={noteToEdit}
+                    onDismiss={() => setNoteToEdit(null)}
+                    onNoteSaved={(updatedNote) => {
+                        setNotes(
+                            notes.map((existingNote) =>
+                                existingNote._id === updatedNote._id
+                                    ? updatedNote
+                                    : existingNote
+                            )
+                        );
+                        setNoteToEdit(null);
+                    }}
+                />
+            )}
+        </Container>
+    );
 }
 
 export default App;
